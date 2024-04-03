62c6f453a710edfa6a45e474a923617529f0a19e
Michael <michael8t6@gmail.com>
2024-04-03 16:43:53 +0100

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./styles/generic_styles.css";
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/types/AddTemplate.ts
@@ -0,0 +1,2 @@
+export type OnSubmit = (e: React.FormEvent) => void;
+export type TableSchema = { name: string; col: string; row: string };
